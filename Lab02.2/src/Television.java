c52ea8cc67460565be174316219afd5524f75805
jdonith <josh.donith@gmail.com>
2022-09-06 11:43:49 -0400

    Initial commit.

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,17 +1,43 @@
+import java.io.StringBufferInputStream;
+
 public class Television {
 
-  String brand;
+  private String brand;
   int volume;
 
-  void turnOn() {
+  public void turnOn() {
     System.out.println("The " + " television is on, with a volume of " + volume);
   }
 
   void turnOff() {
-    System.out.println("The " + brand + " television is off");
+    System.out.println("The " + brand + " television is off");}
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public int getVolume() {
+    return volume;
+  }
+
+  public void setVolume(int volume) {
+    this.volume = volume;
+  }
+
+  public void setBrand() {
 
   }
 
+    public String toString(){
+    return "Television: Brand=" +brand + ",volume" + volume;
 
+    }
 
+  public void setVolume() {
+  }
 }
+
--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -3,18 +3,19 @@ public class TelevisionClient {
   public static void main(String[] args) {
 
       Television tv1 = new Television();
-      tv1.brand = "Samsung";
-      tv1.volume = 25;
+      tv1.setBrand("Samsung");
+      tv1.setVolume(32);
 
       Television tv2 = new Television();
-      tv2.brand = "LG";
-      tv2.volume = 35;
+      tv2.setBrand("LG");
+      tv2.setVolume(35);
 
       tv1.turnOn();
       tv1.turnOff();
 
       tv2.turnOn();
       tv2.turnOff();
-
+System.out.println(tv1.toString());
+System.out.println(tv2);
   }
 }
